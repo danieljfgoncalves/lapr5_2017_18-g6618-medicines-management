4a72d91cf0063f88ac4d1b46e5ba0b2adb5b8347
Diana Godinho da Silva <1151088@isep.ipp.pt>
2018-01-12 08:08:08 +0000

    [TESTS] - database test config

--- config.js
+++ config.js
@@ -3,5 +3,9 @@ module.exports = {
     'logger': {
         'db': 'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5-6618-system-logging',
         'collection': 'request-logs'
+    },
+    'mongoURI':{
+        'testDB':'mongodb://admin:admin@ds141796.mlab.com:41796/lapr5-6618-test-medicines-management'
     }
+
 }
